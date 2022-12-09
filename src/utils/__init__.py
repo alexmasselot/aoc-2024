130e40ea309545e0a0ce1454f7c3c40b582c7f3e
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-12-10 00:28:29 +0100

    d08

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,17 @@
+import numpy as np
+
+
 def read_input(filename: str):
     with open(filename) as fd:
         return [l.replace('\n', '') for l in fd.readlines()]
+
+
+def np_dense_matrix(filename: str):
+    lines = read_input(filename)
+    array = [
+        [int(x) for x in list(l)]
+        for l in lines
+    ]
+
+    return np.asmatrix(array)
+
