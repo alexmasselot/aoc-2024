b657bfe974a1ef7cac542213ad8e75ca2802d836
Alex Masselot <alexandre.masselot@loccitane.com>
2024-12-26 10:36:33 +0100

    day 21 - refactor keypad holds every information

--- src/d21/__init__.py
+++ src/d21/__init__.py
@@ -20,11 +20,45 @@ LAYOUT_ARROW = ' ^A\n<v>'
 # | < | v | > |
 # +---+---+---+
 
-class Keypad():
-    grid: list[list[int]]
+class Keypad:
+    grid: list[list[str]]
+    is_empty_bl: bool
+    sub_paths: dict[str, dict[str, list[str]]]
 
-    def __init__(self, layout: str):
+    def __init__(self, layout: str, is_empty_bl: bool):
         self.grid = [list(l) for l in layout.split('\n')]
+        self.is_empty_bl = is_empty_bl
+        self.build_paths()
+
+    def build_paths(self):
+        """
+        for two keys a and b on the keyboard, build the list of possible paths to go from one to the other.
+        Do not go through the empty position.
+        just make L paths, as staircase patterns wil only be longer.
+        :return:
+        """
+        sub_paths = {}
+        for r_a, line_a in enumerate(self.grid):
+            for c_a, a in enumerate(line_a):
+                if a == ' ':
+                    continue
+                sub_paths[a] = {a: 'A'}
+                for r_b, line_b in enumerate(self.grid):
+                    for c_b, b in enumerate(line_b):
+                        if b == ' ':
+                            continue
+                        d_r = r_b - r_a
+                        d_c = c_b - c_a
+                        if d_r >= 0 and d_c >= 0:  # >v
+                            p = '>' * d_c + 'v' * d_r
+                        if d_r >= 0 > d_c:  # v<
+                            p = 'v' * d_r + '<' * (-d_c)
+                        if d_r < 0 and d_c < 0:  # ^<
+                            p = '^' * (-d_r) + '<' * (-d_c)
+                        if d_r < 0 <= d_c:  # >^
+                            p = '>' * d_c + '^' * (-d_r)
+                        sub_paths[a][b] = p + 'A'
+        self.sub_paths = sub_paths
 
     def type(self, seq: str):
         move = {
@@ -43,62 +77,26 @@ class Keypad():
         return ret
 
 
-def build_path(kp_input_str):
-    sub_path = {}
-    for r_a, line_a in enumerate(kp_input_str.split('\n')):
-        for c_a, a in enumerate(line_a):
-            if a == ' ':
-                continue
-            sub_path[a] = {a: 'A'}
-            for r_b, line_b in enumerate(kp_input_str.split('\n')):
-                for c_b, b in enumerate(line_b):
-                    if b == ' ':
-                        continue
-                    d_r = r_b - r_a
-                    d_c = c_b - c_a
-                    if d_r >= 0 and d_c >= 0:  # >v
-                        p = '>' * d_c + 'v' * d_r
-                    if d_r >= 0 > d_c:  # v<
-                        p = 'v' * d_r + '<' * (-d_c)
-                    if d_r < 0 and d_c < 0:  # ^<
-                        p = '^' * (-d_r) + '<' * (-d_c)
-                    if d_r < 0 <= d_c:  # >^
-                        p = '>' * d_c + '^' * (-d_r)
-                    sub_path[a][b] = p + 'A'
-    return sub_path
-
-
 def command(cs, keypad) -> str:
     ret = ''
     for a, b in zip('A' + cs, cs):
-        ret += keypad[a][b]
+        ret += keypad.sub_paths[a][b]
     return ret
 
 
-def build_numeric_keypad():
-    txt = LAYOUT_NUMERIC
-    return build_path(txt)
-
-
-def build_arrow_keypad():
-    txt = LAYOUT_ARROW
-    return build_path(txt)
-
 
 def command_rec(target: str, level: int = 3):
-    l1 = command(target, numeric_keypad_path)
+    l1 = command(target, numeric_keypad)
     if level == 1:
         return l1
-    l2 = command(l1, arrow_keypad_path)
+    l2 = command(l1, arrow_keypad)
     if level == 2:
         return l2
-    return command(l2, arrow_keypad_path)
+    return command(l2, arrow_keypad)
 
 
-numeric_keypad_path = build_numeric_keypad()
-numeric_keypad = Keypad(LAYOUT_NUMERIC)
-arrow_keypad_path = build_arrow_keypad()
-arrow_keypad = Keypad(LAYOUT_ARROW)
+numeric_keypad = Keypad(LAYOUT_NUMERIC, True)
+arrow_keypad = Keypad(LAYOUT_ARROW, False)
 
 if __name__ == '__main__':
 
